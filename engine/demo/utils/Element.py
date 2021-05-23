b743f74a6af305d12ecb34936873d7af243d790d
LYL <shertheus@gmail.com>
2021-05-23 16:46:25 +0800

    parse frame

---
+++ engine/demo/utils/Element.py
@@ -0,0 +1,62 @@
+class Concept:
+    def __init__(self, _id, name):
+        self.id = _id
+        self.name = name
+        self.related = []
+        self.same = []
+        self.url = ""
+        self.sub = []
+        self.instance = []
+
+    def add_related(self, related):
+        self.related.append(related)
+
+    def set_url(self, url):
+        self.url = url
+
+    def add_sub(self, sub):
+        self.sub.append(sub)
+
+    def add_same(self, sub):
+        self.same.append(sub)
+
+    def add_instance(self, instance):
+        self.instance.append(instance)
+
+
+class Instance:
+    def __init__(self, _id, name, supplement=None):
+        self.id = _id
+        self.name = name
+        self.supplement = supplement
+        self.related = []
+        self.same = []
+        self.url = ""
+        self.comment = ""
+        self.comment_ins = []
+
+    def add_related(self, related):
+        self.related.append(related)
+
+    def set_url(self, url):
+        self.url = url
+
+    def add_same(self, sub):
+        self.same.append(sub)
+
+    def set_comment(self, comment):
+        self.comment = comment
+        # TODO parse comment to get comment_ins
+        # self.comment_ins
+
+
+class Property:
+    def __init__(self, _id, label, fullname=None):
+        self.id = _id
+        self.label = label
+        self.fullname = fullname
+
+# class Noun:
+#     def __init__(self, _id, name):
+#         self.id = _id
+#         self.name = name
---
+++ engine/demo/utils/parse.py
@@ -0,0 +1,66 @@
+def read_concept_list():
+    pass
+
+
+def read_concept_related():
+    pass
+
+
+def read_concept_sameAs():
+    pass
+
+
+def read_concept_url():
+    pass
+
+
+def read_subclass():
+    pass
+
+
+def read_instance_list():
+    pass
+
+
+def read_instanceof():
+    pass
+
+
+def read_instance_related():
+    pass
+
+
+def read_instance_sameAs():
+    pass
+
+
+def read_instance_url():
+    pass
+
+
+def read_property_list():
+    pass
+
+
+def read_instance_text():
+    pass
+
+
+def read_infobox():
+    pass
+
+
+def main():
+    read_concept_list()
+    read_concept_related()
+    read_concept_sameAs()
+    read_concept_url()
+    read_subclass()
+    read_instance_list()
+    read_instanceof()
+    read_instance_related()
+    read_instance_sameAs()
+    read_instance_url()
+    read_property_list()
+    read_instance_text()
+    read_infobox()
