a60544fb6318d8206891986d95cb5335cf8632d8
韩子沛 <hzp18@mails.tsinghua.edu.cn>
2021-05-24 14:12:36 +0800

    数据放ttl文件夹下

--- engine/demo/utils/Element.py
+++ engine/demo/utils/Element.py
@@ -1,3 +1,4 @@
+import re
 class Concept:
     def __init__(self, _id, name):
         self.id = _id
@@ -5,7 +6,7 @@ class Concept:
         self.related = []
         self.same = []
         self.url = ""
-        self.sub = []
+        self.sub = []#该类的子类
         self.instance = []
 
     def add_related(self, related):
@@ -35,7 +36,17 @@ class Instance:
         self.url = ""
         self.comment = ""
         self.comment_ins = []
-
+        self.pro = []
+    
+    def add_pro(self, pro):
+        self.pro.append(pro)
+#新加的设置supplement alias
+    def set_supplement_alias(self,str,f):
+        if f == 1:
+            self.supplement = str
+        else :
+            self.alias = str
+        
     def add_related(self, related):
         self.related.append(related)
 
@@ -46,9 +57,20 @@ class Instance:
         self.same.append(sub)
 
     def set_comment(self, comment):
-        self.comment = comment
         # TODO parse comment to get comment_ins
         # self.comment_ins
+        words = re.findall('\[\[(.*?)\]\]',comment)
+        for w in words :
+            nid = re.findall('[a-z]+[\d]+',w)
+            nw = re.findall('\|.+',w)
+            nnw = nid
+            if len(nw) != 0:
+                nnw[0] = nw[0][1:]
+            comment = comment.replace('[['+ w + ']]', nnw[0])
+            if nid[0] not in self.comment_ins:
+                self.comment_ins.append(nid[0])
+        self.comment = comment
+
 
 
 class Property:
@@ -57,6 +79,9 @@ class Property:
         self.label = label
         self.fullname = fullname
 
+    #新加的设置supplement
+    def set_fullname(self,fullname):
+        self.fullname = fullname
 # class Noun:
 #     def __init__(self, _id, name):
 #         self.id = _id
--- engine/demo/utils/Parser.py
+++ engine/demo/utils/Parser.py
@@ -1,77 +1,224 @@
+# -*- coding: utf-8 -*-
+import io
+import sys
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+
 from Element import Concept, Instance, Property
+import os
+import re
 
 concept_list = {}  # 格式concept["concept_id"] = Concept
 instance_list = {}
-property_lit = {}
+property_list = {}
 
 
-def read_concept_list():
-    pass
 
+path = os.path.dirname(os.path.abspath(__file__)) + '\\ttl'
+def set_concept():
+    with open(path+"\\xlore.concept.list.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[<](.*?)[>]',line)
+            name = re.findall(r'["](.*?)["]',line)
+            if len(name) == 0 :
+                continue
 
-def read_concept_related():
-    pass
+            #print (id[0] + ' ' + name[0])
+            new_concept = Concept(id[0],name[0])
+            concept_list[id[0]] = new_concept
+    f.close()
+    print(1)
+    with open(path+"\\xlore.concept.related.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            concept_list[id[0]].add_related(id[1])
+    f.close()
+    print(2)
+    with open(path+"\\xlore.concept.sameAs.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            concept_list[id[0]].add_same(id[1])
+    f.close()
+    print(3)
+    with open(path+"\\xlore.concept.url.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[<](.*?)[>]',line)
+            url = re.findall(r'["](.*?)["]',line)
 
+            #print (id[0] + ' ' + url[0])
+            concept_list[id[0]].set_url(url[0])
+    f.close()
+    print(4)
 
-def read_concept_sameAs():
-    pass
+def set_instance():
+    print(0)
+    with open(path+"\\xlore.instance.list.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[<](.*?)[>]',line)
+            name = re.findall(r'["](.*?)["]',line)
+            if len(name) == 0 :
+                continue
 
+            #print (id[0] + ' ' + name[0])
+            if id[0] not in instance_list:
+                new_instance = Instance(id[0],name[0])
+                instance_list[id[0]] = new_instance
+            else:
+                if line.find('alias') != -1:
+                    instance_list[id[0]].set_supplement_alias(name[0],0)
+                if line.find('supplement') != -1:
+                    instance_list[id[0]].set_supplement_alias(name[0],1)
 
-def read_concept_url():
-    pass
+    f.close()
+    print(1)
+    with open(path+"\\xlore.instance.related.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            instance_list[id[0]].add_related(id[1])
+    f.close()
+    print(2)
+    with open(path+"\\xlore.instance.sameAs.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            instance_list[id[0]].add_same(id[1])
+    f.close()
+    print(3)
+    with open(path+"\\xlore.instance.text.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[<](.*?)[>]',line)
+            comment = re.findall(r'["](.*?)["]',line)
+            instance_list[id[0]].set_comment(comment[0])
+    f.close()
+    print(4)
+    with open(path+"\\xlore.instance.url.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[<](.*?)[>]',line)
+            url = re.findall(r'["](.*?)["]',line)
+            instance_list[id[0]].set_url(url[0])
+    f.close()
+    print(5)
 
+def set_property():
+    print(0)
+    with open(path+"\\xlore.property.list.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[<](.*?)[>]',line)
+            name = re.findall(r'["](.*?)["]',line)
+            if len(name) == 0 :
+                continue
 
-def read_subclass():
-    pass
+            #print (id[0] + ' ' + name[0])
+            if id[0] not in property_list:
+                new_property = Property(id[0],name[0])
+                property_list[id[0]] = new_property
+            else:
+                property_list[id[0]].set_fullname(name[0])
+    f.close()
+    print(1)
 
+def set_of():
+    with open(path+"\\xlore.infobox.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            name = re.findall(r'["](.*?)["]',line)
+            instance_list[id[0]].add_pro(id[1] + '-' + name[0])
+    f.close()
 
-def read_instance_list():
-    pass
-
-
-def read_instanceof():
-    pass
-
-
-def read_instance_related():
-    pass
-
-
-def read_instance_sameAs():
-    pass
-
-
-def read_instance_url():
-    pass
-
-
-def read_property_list():
-    pass
-
-
-def read_instance_text():
-    pass
-
-
-def read_infobox():
-    pass
-
-
+    with open(path+"\\xlore.subclassOf.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            concept_list[id[1]].add_sub(id[0])
+    f.close()
+    with open(path+"\\xlore.instanceOf.ttl",encoding = 'utf-8') as f:
+        for line in f:
+            if line[0] != '<':
+                continue
+            id = re.findall(r'[a-z]+[\d]+',line)
+            concept_list[id[1]].add_instance(id[0])
+    f.close()
 def main():
-    read_concept_list()
-    read_concept_related()
-    read_concept_sameAs()
-    read_concept_url()
-    read_subclass()
-    read_instance_list()
-    read_instanceof()
-    read_instance_related()
-    read_instance_sameAs()
-    read_instance_url()
-    read_property_list()
-    read_instance_text()
-    read_infobox()
+    set_concept()
+    set_instance()
+    set_property()
+    set_of()
 
-
+def output():
+    count = 0
+    with open(path+"\\concept_list.txt" , 'w') as f:
+        for w in concept_list:
+            if count >=5:
+                break
+            count = count + 1
+            w = concept_list[w]
+            f.write('id:'+ w.id + '|name:' + w.name  + '|url:' + w.url+ '\n|related:' )
+            f.writelines(w.related)
+            f.write('\n|same:') 
+            f.writelines(w.same)  
+            f.write('\n|sub:') 
+            f.writelines( w.sub) 
+            f.write('\n|instance:') 
+            f.writelines(  w.instance) 
+            f.write('\n##################') 
+    f.close()
+    count = 0
+    with open(path+"\\instance_list.txt" , 'w') as f:
+        for w in instance_list:
+            if count >=5:
+                break
+            count = count + 1
+            w = instance_list[w]
+            f.write('id:'+ w.id + '|name:' + w.name 
+            + '|url:' + w.url+ '\n|related:' )
+            f.writelines(w.related)
+            f.write('\n|same:') 
+            f.writelines(w.same)  
+            f.write('\n|comment:') 
+            f.writelines( w.comment) 
+            f.write('\n|comment_ins:') 
+            f.writelines(  w.comment_ins) 
+            f.write('\n|pro:') 
+            f.writelines(  w.pro) 
+            f.write('\n##################') 
+    f.close()
+    count = 0
+    with open(path+"\\property_list.txt" , 'w') as f:
+        for w in property_list:
+            if count >=5:
+                break
+            count = count + 1
+            w = property_list[w]
+            f.write('id:'+ w.id +   '\n##################')
+    f.close()
 if __name__ == "__main__":
     main()
+    output()
+    1
+
+
+a = Instance('bdi3859488','新内站')
+a.set_comment('新内站（ [[bd4154709|朝鲜语]]：）为 [[bd7856391|大韩民国]] [[bd7940985|首尔特别市]] [[bd7941041|首尔地铁6号线]]和 [[bd901454|京春线]]的一个正在建设中车站，位于 [[bd718248|中浪区]]。')
+
+print(a.comment)
