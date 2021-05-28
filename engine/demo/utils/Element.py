2e83ad7322feae6baa4a5a5f16b20bab81501fbf
LYL <shertheus@gmail.com>
2021-05-28 19:11:13 +0800

    bug fix

--- engine/demo/utils/Element.py
+++ engine/demo/utils/Element.py
@@ -1,4 +1,6 @@
 import re
+
+
 class Concept:
     def __init__(self, _id, name):
         self.id = _id
@@ -6,7 +8,7 @@ class Concept:
         self.related = []
         self.same = []
         self.url = ""
-        self.sub = []#该类的子类
+        self.sub = []  # 该类的子类
         self.instance = []
 
     def add_related(self, related):
@@ -26,27 +28,28 @@ class Concept:
 
 
 class Instance:
-    def __init__(self, _id, name, supplement=None, alias=None):
+    def __init__(self, _id, name):
         self.id = _id
         self.name = name
-        self.supplement = supplement
-        self.alias = alias
+        self.supplement = []
+        self.alias = []
         self.related = []
         self.same = []
         self.url = ""
         self.comment = ""
         self.comment_ins = []
         self.pro = []
-    
+
     def add_pro(self, pro):
         self.pro.append(pro)
-#新加的设置supplement alias
-    def set_supplement_alias(self,str,f):
+
+    # 新加的设置supplement alias
+    def set_supplement_alias(self, str, f):
         if f == 1:
-            self.supplement = str
-        else :
-            self.alias = str
-        
+            self.supplement.append(str)
+        else:
+            self.alias.append(str)
+
     def add_related(self, related):
         self.related.append(related)
 
@@ -59,18 +62,17 @@ class Instance:
     def set_comment(self, comment):
         # TODO parse comment to get comment_ins
         # self.comment_ins
-        words = re.findall('\[\[(.*?)\]\]',comment)
-        for w in words :
-            nid = re.findall('[a-z]+[\d]+',w)
-            nw = re.findall('\|.+',w)
+        words = re.findall('\[\[(.*?)\]\]', comment)
+        for w in words:
+            nid = re.findall('[a-z]+[\d]+', w)
+            nw = re.findall('\|.+', w)
             nnw = nid
             if len(nw) != 0:
                 nnw[0] = nw[0][1:]
-            comment = comment.replace('[['+ w + ']]', nnw[0])
+            comment = comment.replace('[[' + w + ']]', nnw[0])
             if nid[0] not in self.comment_ins:
                 self.comment_ins.append(nid[0])
         self.comment = comment
-
 
 
 class Property:
@@ -79,8 +81,8 @@ class Property:
         self.label = label
         self.fullname = fullname
 
-    #新加的设置supplement
-    def set_fullname(self,fullname):
+    # 新加的设置supplement
+    def set_fullname(self, fullname):
         self.fullname = fullname
 # class Noun:
 #     def __init__(self, _id, name):
